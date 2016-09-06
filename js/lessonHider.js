fd79fac7a0f4f58f2b26451825faa4625c6fed8b
Austin Walker <Austin Walker>
2016-09-06 17:26:22 -0600

    W4D2 Project Completed with checkbox

---
+++ js/lessonHider.js
@@ -0,0 +1,55 @@
+angular.module('directivePractice').directive('lessonHider', function(){
+  return {
+    templateUrl: './html/lessonHider.html',
+    restrict:'E',
+    scope: {
+      lesson: '=',
+      dayAlert: '&',
+      checker: '='
+    },
+    controller: function($scope, lessonService) {
+      $scope.getSchedule = lessonService.getSchedule();
+    },
+    link: function(scope, element, attrs) {
+      var checked = false;
+      scope.check = false;
+      scope.checkCheck = function() {
+        if ((element.css) === (element.css('text-decoration', 'checked'))){
+          return 'checked';
+        }
+        else {
+          return '';
+        }
+      };
+      scope.clicked = function(){
+        if (checked) {
+          element.css('text-decoration', 'none');
+          checked= false;
+          scope.check = false;
+        }
+        else {
+          element.css('text-decoration', 'line-through');
+          checked = true;
+          scope.check = true;
+        }
+
+      };
+      scope.getSchedule.then(function(response){
+        scope.schedule = response.data;
+
+        scope.schedule.forEach(function( scheduleDay ){
+          if (scheduleDay.lesson === scope.lesson) {
+            element.css('text-decoration', 'line-through');
+            scope.lessonDay = scheduleDay.weekday;
+            checked = true;
+            scope.check = true;
+            return;
+          }
+        });
+      });
+      // scope.checker = function () {
+      //   element.css('text-decoration', 'line-through');
+      // };
+    }
+  };
+});
